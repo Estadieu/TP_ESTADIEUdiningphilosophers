3b7ea46a42065ed9e855fee97b252e258ed7b19b
Estadieu <jean.estadieu@etud.univ-jfc.fr>
2025-03-21 20:19:18 +0100

    JE:Versions2

--- src/main/java/diningphilosophers/ChopStick.java
+++ src/main/java/diningphilosophers/ChopStick.java
@@ -1,23 +1,25 @@
 package diningphilosophers;
 
+// ESTADIEU JEAN 21/03/2025
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class ChopStick {
-    //test
+    
     private static int stickCount = 0;
     private boolean iAmFree = true;
     private final int myNumber;
 
+    //Ajout du verou explicite
+    //Permet de controler l'accèes concurrent à une ressource partagées entre plusieurs threads 
     private final static Lock lock = new ReentrantLock();
-
-
     public ChopStick() {
         myNumber = ++stickCount;
     }
 
     public boolean tryTake(int delay) throws InterruptedException {
         if (!iAmFree) {
+            //TryLock (tente d'acquérir mais ne bloque pas)
             boolean locked = lock.tryLock(300, java.util.concurrent.TimeUnit.MILLISECONDS);
             if (!locked) // Toujours pas libre, on abandonne
             {
@@ -25,6 +27,8 @@ public class ChopStick {
             }
         }
         else{
+            //Verouille
+            //Un seul thread à la fois pourra entrer dans la section critique, les autres devrons attendre leur tour 
             lock.lock();
         }
         iAmFree = false;
@@ -38,6 +42,7 @@ public class ChopStick {
             System.out.println("Stick " + myNumber + " Released");
         }
         finally {
+            // Ouvrir le verrou 
             lock.unlock();
         }
     }
